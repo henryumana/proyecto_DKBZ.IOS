1eeb68d9f871ff62591d8bd7dee11bb895753872
henryumana <henryvalladores2110@gmail.com>
2019-10-03 16:50:19 -0500

    ios test

---
+++ Proyecto_DKBZ.IOS/ViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  Proyecto_DKBZ.IOS
+//
+//  Created by Invitado on 10/2/19.
+//  Copyright © 2019 Invitado. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+ class ViewController: UIViewController {
+     
+    
+  @IBOutlet weak var inglesButton: UILabel!
+  @IBOutlet weak var españolButton: UIButton!
+  @IBOutlet weak var portuguesButton: UIButton!
+    
+    private var webView: WKWebView?
+
+    override func loadView() {
+        webView = WKWebView()
+
+        //If you want to implement the delegate
+        //webView?.navigationDelegate = self
+
+        view = webView
+    }
+    
+
+    @IBAction func inglesButtonPressed(_ sender: Any) {
+        guard let url = URL(string: "https://dkbz-dummy-api.herokuapp.com?lang=eng")else{
+            fatalError("Error getting URL")
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let Session = URLSession.shared
+        Session.dataTask(with: url){(data, response, error) in
+            if let data = data{
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dict = json as? [String: Any]{
+                        if  let  value = dict["value"] as? String{
+                            DispatchQueue.main.async {
+                                self.inglesButton.text = value
+                                
+                            }
+                        }
+                    }
+                }catch{
+                    print(error)
+                }
+               
+            }
+            }.resume()
+        presentacion(answers:"eng")
+    }
+   
+  
+   
+    @IBAction func español(_ sender: Any) {
+     guard let url = URL(string: "https://dkbz-dummy-api.herokuapp.com?lang=esp")else{
+         fatalError("Error getting URL")
+     }
+     var request = URLRequest(url: url)
+     request.httpMethod = "GET"
+     let Session = URLSession.shared
+     Session.dataTask(with: url){(data, response, error) in
+         if let data = data{
+             do{
+                 let json = try JSONSerialization.jsonObject(with: data, options: [])
+                 if let dict = json as? [String: Any]{
+                     if  let  value = dict["value"] as? String{
+                         DispatchQueue.main.async {
+                             self.inglesButton.text = value
+                             
+                         }
+                 }
+             }
+         }catch{
+             print(error)
+         }
+        
+     }
+     }.resume()
+        presentacion(answers:"esp")
+    }
+    
+    @IBAction func portugues(_ sender: Any) {
+        guard let url = URL(string: "https://dkbz-dummy-api.herokuapp.com?lang=por")else{
+            fatalError("Error getting URL")
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let Session = URLSession.shared
+        Session.dataTask(with: url){(data, response, error) in
+            if let data = data{
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dict = json as? [String: Any]{
+                        if  let  value = dict["value"] as? String{
+                            DispatchQueue.main.async {
+                                self.inglesButton.text = value
+                                
+                            }
+                        }
+                    }
+                }catch{
+                    print(error)
+                }
+               
+            }
+            }.resume()
+        presentacion(answers:"por")
+    }
+    
+    func presentacion(answers:String){
+        super.viewDidLoad()
+        
+        if let url = URL(string: "https://dkbz-dummy-api.herokuapp.com?lang=" + answers) {
+            let req = URLRequest(url: url)
+            webView?.load(req)
+        }
+    }
+    
+    
+}
+    
---
+++ Proyecto_DKBZ.IOSTests/Proyecto_DKBZ_IOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Proyecto_DKBZ_IOSTests.swift
+//  Proyecto_DKBZ.IOSTests
+//
+//  Created by Invitado on 10/2/19.
+//  Copyright © 2019 Invitado. All rights reserved.
+//
+
+import XCTest
+@testable import Proyecto_DKBZ_IOS
+
+class Proyecto_DKBZ_IOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
